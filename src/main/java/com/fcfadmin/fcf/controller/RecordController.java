e70993c4d2a245430b866df1e2567566dffeee80
Luan Luiz <luanluizg@gmail.com>
2022-05-29 23:27:25 -0300

    rbc

--- src/main/java/com/fcfadmin/fcf/controller/RecordController.java
+++ src/main/java/com/fcfadmin/fcf/controller/RecordController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/records")
+@RequestMapping("/api/records")
 public class RecordController {
 
     private final RecordService recordService;
